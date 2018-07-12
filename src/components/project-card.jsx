8c1cacb4a720380f51a0599d2de35d36807b1b39
victorblevi <victorbatistalevi@gmail.com>
2018-07-12 18:46:16 -0300

    project-card componentized.

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,28 +1,35 @@
 import React from 'react'
 import Grid from '../components/grid'
-
 export default props => (
 
     <div className='container__row project-card'>
-        <div className='container__col-4'>
-            {/* <img src={props.img} alt={props.alt} className='img-adjust'></img> */}
-            <h1> aaaaaaaaaa</h1>
+        <div className='container__col-5'>
+            <figure>
+                <img src={props.img} className='img-adjust-div' alt={props.alt} />
+            </figure>
         </div>
-        <div className='container__col-8'>
-            <h3>Gelmeza – Sistema de Custo</h3>
-            <p> Este projeto consistiu em desenvolver o fluxo de cadastro de produtos, funcionários, fornecedores e custos a partir de uma
-                    planilha de excel para um sistema web. O sistema foi desenvolvido</p>
-            <p>Tecnologias utilizadas:</p>
+        <div className='container__col-7 padding-container flex'>
+            <h3>{props.title}</h3>
+            <p>{props.desc}</p>
+            <p className='text-stack'>Tecnologias Utilizadas:</p>
             <div className='list-stack'>
                 <ul>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
-                    <li>Bootstrap 3</li>
+                    {
+                        props.stack.map((item, index) => (
+                            <li key={index}><i className='fa fa-caret-right' /> {item}</li>
+                        ))
+                    }
                 </ul>
+            </div>
+            <div className='button-acess'>
+                {
+                    props.btns.map((btn, index) => (
+                        <a className='button' target='_blank'
+                            href={btn.link} key={index}>
+                            <i className={`fa fa-${btn.icon} fa-2x`} />
+                        </a>
+                    ))
+                }
             </div>
         </div>
     </div>
--- src/screens/portifolio.jsx
+++ src/screens/portifolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectCard from '../components/project-card'
 
+
 export default props => (
     <section id='portifolio' className='bg-portifolio content-page'>
         <h2>
@@ -9,11 +10,83 @@ export default props => (
         <h3>
             Meus últimos trabralhos
         </h3>
-        <div className='container--fluid'>
+        <div className='container'>
             <ProjectCard
-                img='https://francisgregori.com.br/wp-content/uploads/2018/05/dce-featured-1-500x500.jpg'
-                alt='Cacau Show'
+                img='https://image.ibb.co/jPseu8/cacaushow.png'
+                alt='CacauShow'
+                title='Cacau Show - Landings Pages'
+                desc='Lorem ipsum dolor sit amet consectetur adipisicing elit.antium, fugiat totam nesciunt neque consequuntur qui? Ratione, minima atque molestias vel sunt modi autem harum voluptatibus.'
+                stack={['html', 'CSS3', 'JavaScript']}
+                btns={[
+                    {   icon: 'github',
+                        link: 'http://www.google.com'},
+                    {   icon: 'github',
+                        link: 'http://www.google.com'}
+                    ]}
             />
+
+            <ProjectCard
+                img='https://preview.ibb.co/mA9jBo/ugolf.png'
+                alt='Ugolf'
+                title='UGOLF - Eccomerce'
+                desc='Lorem ipsum dolor sit amet consectetur adipisicing elit.antium, fugiat totam nesciunt neque consequuntur qui? Ratione, minima atque molestias vel sunt modi autem harum voluptatibus.'
+                stack={['CMS - Loja Integrada', 'CSS3']}
+                btns={[
+                    {   icon: 'github',
+                        link: 'http://www.google.com'},
+                    {   icon: 'github',
+                        link: 'http://www.google.com'}
+                    ]}
+            />
+
+            <ProjectCard
+                img='https://preview.ibb.co/nJHCWo/Screenshot_1.png'
+                alt='EmailIfood'
+                title='iFoodShop - Email Market Responsivos'
+                desc='Lorem ipsum dolor sit amet consectetur adipisicing elit.antium, fugiat totam nesciunt neque consequuntur qui? Ratione, minima atque molestias vel sunt modi autem harum voluptatibus.'
+                stack={['HTML', 'CSS3', 'MJML']}
+                btns={[
+                    {   icon: 'github',
+                        link: 'http://www.google.com'},
+                    {   icon: 'github',
+                        link: 'http://www.google.com'}
+                    ]}
+            />
+
+            <ProjectCard
+                img='https://image.ibb.co/m3sFHT/emg.png'
+                alt='EMG'
+                title='EMG -  Gerador de Email Market Responsivos'
+                desc='Projeto desenvolvido para automatizar criação de
+                email-market para agencia de comunicação. Foi desenvolvido para atender a criação de emails para o iFoodShop.
+                Basicamente como funciona?
+                A entrada é uma planilha excel.
+                Busca informações no site do iFoodShop referente ao produto da planilha.
+                Gera os HTMLs de forma dinâmica.'
+                stack={['NodeJS', 'JavaScript']}
+                btns={[
+                    {   icon: 'globe',
+                        link: 'https://www.youtube.com/watch?v=vqjanNO7NL4&feature=youtu.be'},
+                    ]}
+            />
+
+                <ProjectCard
+                img='https://preview.ibb.co/hfwRu8/crud.png'
+                alt='CRUD - REACT'
+                title='CRUD -  CRUD em ReactJS'
+                desc='Projeto desenvolvido para automatizar criação de
+                email-market para agencia de comunicação. Foi desenvolvido para atender a criação de emails para o iFoodShop.
+'
+                stack={['NodeJS', 'JavaScript']}
+                btns={[
+                    {   icon: 'github',
+                        link: 'https://github.com/victorblevi/crud-reactJS'},
+                        {
+                        icon: 'globe',
+                        link: 'https://www.youtube.com/watch?v=vqjanNO7NL4&feature=youtu.be'},
+                    ]}
+            />
+
         </div>
     </section>
 )
